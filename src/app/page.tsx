755035a6856eca36ec2f2a150c5a0100e9ec2384
ikorchev <ivaylo.korchev@spacenk.com>
2024-02-08 11:48:43 +0000

    rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="p-12">
       <h1 className="text-4xl text-center mt-5">
-        SpaceNK Tools
+        Tools
       </h1>
       <div className="grid grid-cols-1  gap-5 container max-w-md mt-5 mx-auto place-items-center">
         <Link href="/category-footer" className="bg-gray-700 p-5 text-center text-white hover:bg-gray-800 w-full h-full">Category Footer Builder</Link>
