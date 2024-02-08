572a30dec8a2ad4a783b2cb0b27c6b6202e31554
ikorchev <ivaylo.korchev@spacenk.com>
2024-02-08 12:16:11 +0000

    fix description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tools",
-  description: "Tools",
+  description: "Tools by Ivaylo Korchev",
 };
 
 export default function RootLayout({
